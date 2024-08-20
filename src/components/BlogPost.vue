a388d4e1f611ed68ef41b71081c63d92c1cfec1d
justinornelas10 <jose.ornelas@cceo.com.mx>
2024-08-20 15:25:36 -0600

    feat: :sparkles: Do a nav-page using bootstrap and an fake API

---
+++ src/components/BlogPost.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+defineProps({
+    title: String,
+    id:  Number,
+    body: {
+        type: String,
+        default: "Sin descripción",
+    },
+})
+
+const emit = defineEmits(['changeFav'])
+
+</script>
+
+<template>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">{{ id }}.- {{ title }}</h5>
+            <p>{{ body }} </p>
+            <button @click="$emit('changeFav', title)" class="btn btn-outline-primary">Favorito</button>
+        </div>
+    </div>
+</template>
---
+++ src/components/PaginatePost.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { computed } from 'vue';
+
+
+defineProps(['start','end','maxLength'])
+const emit = defineEmits(['next', 'prev'])
+
+// const checkPrev = computed(() => {
+//     if (start.value === 0) {
+//         return 'true'
+//     }else{
+//         return 'false'
+//     }
+// })
+
+</script>
+
+<template>
+    <div class="btn-group" role="group" aria-label="Basic Example">
+        <button 
+        @click="emit('prev')" :disabled="start <= 0"
+        type="button" class="btn btn-outline-primary">
+            Previous
+
+        </button>
+        <button 
+        @click="emit('next')" :disabled="end >= maxLength"
+        type="button" class="btn btn-primary">
+            Next
+
+        </button>
+    </div>
+</template>
